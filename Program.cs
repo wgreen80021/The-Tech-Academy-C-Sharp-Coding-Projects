dfeaef074d7501a9c825e7691f5dc18d75ce9956
WA <45290206+wgreen80021@users.noreply.github.com>
2019-03-23 03:35:31 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloFinalProj
+{
+   
+    class Program
+    {
+        class Message
+        {
+            public int id { get; set; }
+            public string strMessage { get; set; }
+            public DateTime MsgWhen { get; set; }
+        }
+
+        class MsgContext : DbContext
+        {
+            public MsgContext() : base(@"Data Source=DESKTOP-BE2OVPM\SQLEXPRESS;Initial Catalog=FinalHello;Integrated Security=True") { }
+            public DbSet<Message> Messages { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+
+            var message = new Message
+            {
+                strMessage = "Hello World",
+                MsgWhen = DateTime.Now
+
+            };
+
+            var msgContext = new MsgContext();
+            msgContext.Messages.Add(message);
+            msgContext.SaveChanges();
+        }
+    }
+}
